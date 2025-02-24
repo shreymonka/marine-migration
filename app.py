fe1040f1f296910c3f55ec524a5187657658161c
Spidey0819 <goswamidhruv302031@gmail.com>
2025-02-23 22:38:57 -0400

    about

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="Humpback Migration Dashboard", layout="wide")
 # API Configuration
 API_URL = "https://data.oceannetworks.ca/api/scalardata/device"
 DEFAULT_TIME_RANGE = "Past 10 Minutes"
-API_TOKEN = "492d6df3-c559-4e2b-8da0-1263e326ae1f"  
+API_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  
 DEVICE_CODES = {
     "standard": "SBEDSPHOXV2SN7212038",
     "fluorometer": "TURNERCYCLOPS7F-900143"
